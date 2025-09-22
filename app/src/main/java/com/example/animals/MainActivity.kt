d2f71c05c44cf8fbaaf6185592b70260f12c4c0e
tarik-git <tarik6b@hotmail.com>
2025-09-22 23:34:39 +0200

    feat: Add starting project

--- app/src/main/java/com/example/animals/MainActivity.kt
+++ app/src/main/java/com/example/animals/MainActivity.kt
@@ -3,44 +3,107 @@ package com.example.animals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
+import com.example.animals.model.IconItem
 import com.example.animals.ui.theme.AnimalsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             AnimalsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize()
+                        .windowInsetsPadding(WindowInsets.statusBars)
+                        .windowInsetsPadding(WindowInsets.navigationBars),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val list = listOf(
+                        IconItem(
+                            iconRes = R.drawable.dog,
+                            title = "Dog",
+                            aliasName = "$packageName.MainActivityDog"
+                        ),
+                        IconItem(
+                            iconRes = R.drawable.cat,
+                            title = "Cat",
+                            aliasName = "$packageName.MainActivityCat"
+                        )
+                    )
+                    LazyColumn (
+                        modifier = Modifier.fillMaxSize(),
+                        contentPadding = PaddingValues(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        items( list ) { item -> 
+                            Card (
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 2.dp, bottom = 2.dp)
+                                    .clickable {
+                                               setIcon(item.aliasName)
+                                    },
+                                elevation = CardDefaults.cardElevation(4.dp)
+                            ) {
+                                Row (
+                                    modifier = Modifier.padding(8.dp),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                ){
+                                    Image(
+                                        modifier = Modifier
+                                            .width(55.dp)
+                                            .height(55.dp),
+                                        painter = painterResource(id = item.iconRes),
+                                        contentDescription = item.title,
+                                    )
+                                    Text(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(start = 8.dp, end = 8.dp),
+                                        text = item.title,
+                                        fontSize = 22.sp,
+                                        fontWeight = FontWeight.SemiBold
+                                    )
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    fun setIcon(aliasName: String) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimalsTheme {
-        Greeting("Android")
     }
+
 }
---
+++ app/src/main/java/com/example/animals/model/IconItem.kt
@@ -0,0 +1,7 @@
+package com.example.animals.model
+
+data class IconItem (
+    val iconRes: Int,
+    val title: String,
+    val aliasName: String
+)
